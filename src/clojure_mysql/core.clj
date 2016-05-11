a0b9c0ef9f1847cadb37201c4946917a43ea8e05
CNwwsun <wwsun@thoughtworks.com>
2016-05-12 07:49:10 +0800

    Clojure Mysql first commit

---
+++ src/clojure_mysql/core.clj
@@ -0,0 +1,18 @@
+(ns clojure-mysql.core
+  (:require [clojure.java.jdbc :as sql]))
+
+(def db {:classname "com.mysql.jdbc.Driver"
+         :subprotocol "mysql"
+         :subname "//localhost:3306/ihakula_tea"
+         :user "root"
+         :password "Wayde191!"})
+
+(defn list-users [table-name]
+  (sql/with-connection db
+    (sql/with-query-results rows
+      ["select * from ih_product"]
+      (println rows))))
+
+(defn -main
+  [& args]
+  (list-users "Hello, World!"))
